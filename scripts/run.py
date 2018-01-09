9ffb4f4023dd5b66531bf9fae56b4f6d7f6864f6
Ruhee Dewji <ruhee@twg.io>
2018-01-09 14:50:04 -0500

    WIP

--- scripts/run.py
+++ scripts/run.py
@@ -24,27 +24,28 @@ with open(os.path.join(dirname, 'reactions.json')) as f:
   reactions = json.load(f)
 
 cleaned = []
-unique = [{'name': 'hugging_face', 'count': 0}]
+unique = [{'text': 'hugging_face', 'value': 0}]
 
 for i, item in enumerate(reactions):
-  # if i < 50:
   for j, reaction in enumerate(item):
     name = reaction['name'].split('::')[0]
     count = reaction['count']
-    cleaned.append({ 'name': name, 'count': count })
+    cleaned.append({ 'text': name, 'value': count })
 
 
 for i, item in enumerate(cleaned):
-  emoji = item['name']
-  count = int(item['count'])
+  emoji = item['text']
+  count = int(item['value'])
 
-  obj = {'name':emoji, 'count': count}
-  if not any(d['name'] == emoji for d in unique):
+  obj = {'text':emoji, 'value': count}
+  if not any(d['text'] == emoji for d in unique):
     unique.append(obj)
   else:
     for j, obj in enumerate(unique):
-      if obj['name'] == emoji:
-        obj['count'] += count
+      if obj['text'] == emoji:
+        obj['value'] += count
 
-unique_sorted = sorted(unique, key=lambda k: k['count'], reverse=True)
-json.dump(unique_sorted, open(os.path.join(dirname, 'tidied.json'), 'w'), indent=1)
+unique_sorted = sorted(unique, key=lambda k: k['value'], reverse=True)[0:30]
+
+src_dirname = os.path.dirname(os.path.dirname(__file__))
+json.dump(unique_sorted, open(os.path.join(src_dirname, 'src/tidied.json'), 'w'), indent=1)
--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './tidied.json'
+import BarChart from 'react-bar-chart'
+
+const margin = {top: 20, right: 20, bottom: 200, left: 100};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">reactji-tally</h1>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">reactji-tally</h1>
         </header>
-        <p className="App-intro">
-          Hi!
-        </p>
+        <BarChart
+          ylabel='Frequency'
+          width={800}
+          height={600}
+          data={data}
+          margin={margin} />
       </div>
     );
   }
