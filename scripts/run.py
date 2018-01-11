ab9717d43192e6347fb6c979c2421b07015e27d3
Ruhee Dewji <ruhee@twg.io>
2018-01-11 11:46:41 -0500

    Clean up python script, etc

--- scripts/run.py
+++ scripts/run.py
@@ -24,28 +24,29 @@ with open(os.path.join(dirname, 'reactions.json')) as f:
   reactions = json.load(f)
 
 cleaned = []
-unique = [{'text': 'hugging_face', 'value': 0}]
+unique = []
 
 for i, item in enumerate(reactions):
   for j, reaction in enumerate(item):
     name = reaction['name'].split('::')[0]
     count = reaction['count']
-    cleaned.append({ 'text': name, 'value': count })
+    cleaned.append({ 'text': name, 'value': count }) # react-bar-graph requires these specific keys to work
 
 
 for i, item in enumerate(cleaned):
-  emoji = item['text']
+  emoji_name = item['text']
   count = int(item['value'])
 
-  obj = {'text':emoji, 'value': count}
-  if not any(d['text'] == emoji for d in unique):
+  obj = {'text': emoji_name, 'value': count}
+  if not any(d['text'] == emoji_name for d in unique):
     unique.append(obj)
   else:
     for j, obj in enumerate(unique):
-      if obj['text'] == emoji:
+      if obj['text'] == emoji_name:
         obj['value'] += count
 
 unique_sorted = sorted(unique, key=lambda k: k['value'], reverse=True)[0:30]
 
 src_dirname = os.path.dirname(os.path.dirname(__file__))
 json.dump(unique_sorted, open(os.path.join(src_dirname, 'src/tidied.json'), 'w'), indent=1)
+os.remove(os.path.join(dirname, 'reactions.json'))
