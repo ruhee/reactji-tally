4c780a649c5344e509b21b2cb6a512ea4a7d0ea1
Ruhee Dewji <ruhee@twg.io>
2018-01-12 15:54:03 -0500

    Emoji!!!!

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import data from './tidied.json'
 import BarChart from 'react-bar-chart'
-
+import nameMap from 'emoji-name-map';
 const margin = {top: 20, right: 20, bottom: 200, left: 100};
 
 class App extends Component {
   render() {
+    data.forEach((item) => {
+      const emoji = nameMap.get(item.text);
+      if (emoji) {
+        item.text = emoji;
+      }
+    })
     return (
       <div className="app">
         <header className="app-header">
